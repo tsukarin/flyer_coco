621eeddc864c2e9b7cb19829c2562b22fb0d34b0
raito23 <s2122058@stu.musashino-u.ac.jp>
2022-09-14 06:09:49 +0900

    変更

--- index.php
+++ index.php
@@ -51,9 +51,9 @@ if($aflag==0){
  ?>
  </br>
 
- <a href="./gupload.php">
- <i class="bi bi-door-open-fill"></i>
- </a>
+ <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16" href="./gupload.php">
+  <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+</svg>
 
  <div class="input-group">
           <form method="post" action="./search.php">
