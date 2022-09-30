a02fce768f7209f6bfc60ec0036e165561d19570
raito23 <s2122058@stu.musashino-u.ac.jp>
2022-09-30 16:39:37 +0900

    l

---
+++ mypage.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+if (isset($_SESSION['ems'])) {
+  $ems=$_SESSION['ems'];
+}
+if (isset($_SESSION['pws'])) {
+  $pws=$_SESSION['pws'];
+}
+if (isset($_POST['emf'])){$ems=$_POST['emf'];}
+if (isset($_POST['pwf'])){$pws=$_POST['pwf'];}
+$aflag=0;
+if (isset($ems) &&isset($pws)){
+  $sql="select * from phpua where email='". $ems . "';";
+  $dbconn = pg_connect("host=localhost dbname=raito23 user=raito23 password=0QHxOR5a")
+    or die('Could not connect: ' . pg_last_error());
+  $result = pg_query($sql) or die('Query failed: ' . pg_last_error());
+  if(pg_num_rows($result)==1){
+    $row = pg_fetch_row($result);
+    if (password_verify($pws, $row[3])){
+      $_SESSION['ems']=$ems;
+      $_SESSION['pws']=$pws;
+      $uid=$row[0];
+      $aflag=1;
+    }
+  }
+}
+if($aflag==0){
+  header('location: ./login.html');
+}
+?>
+
+<html>
+<head>
+  <title>
+    login
+  </title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+</head>
+
+
+<body>
+    <a href="index.php">トップページへ</a>
+    <?php
+    //お気に入り  
+    //投稿
+    //アーカイブ
+    ?>
+
+</body>
+</html>
